beabd14acedb8360e3995ae2e51261a46e5b5b8d
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2023-06-06 20:38:53 -0300

    Update Instalador.bat
    adicionado atualizador Firefox

--- Instalador.bat
+++ Instalador.bat
@@ -1,8 +1,6 @@
 @echo off
-
 set "startingFolder=%cd%"
-
-
+echo %startingFolder%
 
 echo Buscando atualizacoes windows...
 wuauclt /detectnow
@@ -12,34 +10,39 @@ echo instalando Chocolatey : o responsavel por instalar nossos pacotes...
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 
+
 echo instalando Firefox...
 chocho install firefox -y
 
+timeout 1
 echo Configurando o Firefox...
-cd "%APPDATA%\Mozilla\Firefox\Profiles"
 
-set copycmd=/y
 start "browser" /d "C:\Program Files\Mozilla Firefox" firefox.exe
-timeout 7
-
-
-c:
-cd %userprofile%\AppData\Roaming\Mozilla\Firefox\
+timeout 5
+taskkill /IM firefox.exe 2>nul
+cd %appdata%\Mozilla\Firefox\
 for /f "tokens=1,2 delims=/" %%i in ('findstr /l ".default" profiles.ini') do call set var1=%%j
 cd profiles\%var1%
 
+set "finalFolder=%cd%"
+
+if exist %startingFolder%\Firefox configuration files\prefs.js (
+   echo copiando Prefs.js
+   copy "%startingFolder%\Firefox configuration files\prefs.js" %finalFolder%
+) 
+if exist %startingFolder%\Firefox configuration files\places.sqlite (
+   echo copiando SQLITE
+   copy "%startingFolder%\Firefox configuration files\places.sqlite" %finalFolder%
+)
+
+echo Fim configuracao Firefox.
 
 
 
 
-set copycmd=/y
-start "browser" /d "C:\Program Files\Mozilla Firefox" firefox.exe
-timeout 7
-c:
-cd %userprofile%\AppData\Roaming\Mozilla\Firefox\
-for /f "tokens=1,2 delims=/" %%i in ('findstr /l ".default" profiles.ini') do call set var1=%%j
-cd profiles\%var1%
-if exist h:\backupbookmarks\places.sqlite copy h:\backupbookmarks\places.sqlite
+
+
+
 
 
 
